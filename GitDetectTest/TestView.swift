0fdf725a83e4bd5ae57b417875b3e418d5d9e8db
TenyaVcore <090.sket@gmail.com>
2024-12-13 11:38:15 +0900

    TestView追加

---
+++ GitDetectTest/TestView.swift
@@ -0,0 +1,18 @@
+//
+//  TestView.swift
+//  GitDetectTest
+//
+//  Created by cw-nobuya.tagawa on 2024/12/13.
+//
+
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+#Preview {
+    TestView()
+}
