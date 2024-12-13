7293a323ae7c811fbe2befda883b417eb86dd114
TenyaVcore <090.sket@gmail.com>
2024-12-13 12:02:50 +0900

    testViewツイアk

---
+++ GitDetectTest/Test2View.swift
@@ -0,0 +1,18 @@
+//
+//  Test2View.swift
+//  GitDetectTest
+//
+//  Created by cw-nobuya.tagawa on 2024/12/13.
+//
+
+import SwiftUI
+
+struct Test2View: View {
+    var body: some View {
+        Text("Test2")
+    }
+}
+
+#Preview {
+    Test2View()
+}
---
+++ GitDetectTest/Test3View.swift
@@ -0,0 +1,18 @@
+//
+//  Test3View.swift
+//  GitDetectTest
+//
+//  Created by cw-nobuya.tagawa on 2024/12/13.
+//
+
+import SwiftUI
+
+struct Test3View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Test3View()
+}
