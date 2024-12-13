e4ed74bbcaa7351c1cbdfac9796c6dfba3c8dbac
TenyaVcore <090.sket@gmail.com>
2024-12-13 10:54:48 +0900

    Initial Commit

---
+++ GitDetectTest/GitDetectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitDetectTestApp.swift
+//  GitDetectTest
+//
+//  Created by cw-nobuya.tagawa on 2024/12/13.
+//
+
+import SwiftUI
+
+@main
+struct GitDetectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
